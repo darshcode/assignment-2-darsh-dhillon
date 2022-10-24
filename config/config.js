2124db95fecab215889403d2d37590ed133d5a88
darshcode <darsh.dhillon@live.com>
2022-10-23 20:02:04 -0400

    again

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 export const Secret = "MySecret";
 // export const MongoURI  = "mongodb://127.0.0.1/media"
 export const MongoURI  = "mongodb+srv://testUser:testPass>@cluster-1.y3mynlu.mongodb.net/media?retryWrites=true&w=majority"
-//mongodb+srv://testUser:<password>@cluster-1.y3mynlu.mongodb.net/?retryWrites=true&w=majority
+
